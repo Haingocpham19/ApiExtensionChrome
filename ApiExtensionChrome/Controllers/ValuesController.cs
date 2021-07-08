a5f01f14634863b4b07cf6d8482640c6447f9305
Haingocpham19 <haingocpham@hotmail.com>
2021-07-08 21:09:31 +0700

    Add project files.

---
+++ ApiExtensionChrome/Controllers/ValuesController.cs
@@ -0,0 +1,74 @@
+﻿using ApiExtensionChrome.ModelEx;
+using ApiExtensionChrome.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiExtensionChrome.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        logindbContext context = new logindbContext();
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public List<AdminLogin> ListAll()
+        {
+            return new AdminLoginDao().ListUser();
+        }
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public List<AdminLogin> Edit(int id)
+        {
+            return new AdminLoginDao().GetUserById(id);
+        }
+        // POST api/<ValuesController>
+        [HttpPost]
+        public bool Create([FromBody] AdminLogin value)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new AdminLoginDao();
+                long id = dao.Insert(value);
+                if (id > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public bool Update([FromRoute]int id,[FromBody] AdminLogin value)
+        {           
+            var existingUser = context.AdminLogin.FirstOrDefault(x => x.Id == id);
+            if (existingUser != null)
+            {
+                value.UserName = value.UserName;
+                value.Password = value.Password;
+                context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public bool Delete(int id)
+        {
+            AdminLoginDao dao = new AdminLoginDao();
+            if (dao.Delete(id)) {
+                return true;
+            };
+            return false;
+        }
+    }
+}
---
+++ ApiExtensionChrome/Models/logindbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ApiExtensionChrome.Models
+{
+    public partial class logindbContext : DbContext
+    {
+        public logindbContext()
+        {
+        }
+
+        public logindbContext(DbContextOptions<logindbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<AdminLogin> AdminLogin { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=THINKPAD-T460;Initial Catalog=logindb;Integrated Security=True;Pooling=False;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AdminLogin>(entity =>
+            {
+                entity.Property(e => e.Password).HasMaxLength(50);
+
+                entity.Property(e => e.UserName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
